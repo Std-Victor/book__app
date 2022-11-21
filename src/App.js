1a014f755f35d22b0c4ce7b6db1cd10be79b839d
Std-Victor <smallvictor220@gmail.com>
2022-11-21 04:22:15 +0100

    control

--- src/App.js
+++ src/App.js
@@ -1,169 +1,127 @@
-import React from "react";
-import { useState } from "react";
-
-import Button from "./components/Button/Button";
-import data from "./Assets/Data.json";
-
+import React, { useState } from "react";
 import "./App.css";
 
-const App = () => {
-  const [control, setControl] = useState({
-    value: "",
-    style: "",
-    icon: "",
-    active: false,
-    disabled: false,
-  });
-  const [controls, setControls] = useState([]);
+export default function App() {
+  const [stagiaires, setStagiaires] = useState([
+    { Nom: "Ali", NoteBac: 13, Option: "DEV" },
+    { Nom: "Ahmed", NoteBac: 11, Option: "ID" },
+    { Nom: "Yassine", NoteBac: 16, Option: "ID" },
+    { Nom: "Mohammed", NoteBac: 14, Option: "DEV" },
+    { Nom: "Alae", NoteBac: 10, Option: "Multimedia" },
+    { Nom: "Mohammed", NoteBac: 15, Option: "DEV" },
+  ]);
+  const [minNote, setMinNote] = useState();
+  const [moyenne, setMoyenne] = useState(0);
+  const [txt, setTxt] = useState("");
+  const [error, setError] = useState();
 
-  const handleChange = (e) =>
-    setControl((prev) => ({
-      ...prev,
-      [e.target.name]: e.target.value,
-    }));
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-    const data = new FormData(e.target);
-    const data_value = Object.fromEntries(data.entries());
-    e.target.reset();
-    setControl({
-      value: "",
-      style: "",
-      icon: "",
-      active: false,
-      disabled: false,
-    });
-    return setControls((prev) => [
-      ...prev,
-      {
-        ...data_value,
-        index:
-          +data_value.nbr +
-          (prev.length !== 0 ? prev[prev.length - 1].index : 0),
-      },
-    ]);
-  };
+  const handleNote = (e) =>
+    setMinNote(
+      stagiaires
+        .filter((item) => item.Option === e.target.value)
+        .sort((a, b) => a.NoteBac - b.NoteBac)[0].NoteBac
+    );
 
   return (
-    <div className="container">
-      <form onSubmit={onSubmit}>
-        <div className="form__group">
-          <label htmlFor="" className="form__group__title">
-            Value
+    <div className="container__">
+      <div className="stagiaire__table">
+        <table border={1}>
+          <thead>
+            <tr>
+              <th>Nom</th>
+              <th>Node Bac</th>
+              <th>Option</th>
+            </tr>
+          </thead>
+          <tbody>
+            {stagiaires.map((stg, index) => (
+              <tr key={index}>
+                <td>{stg.Nom}</td>
+                <td>{stg.NoteBac}</td>
+                <td>{stg.Option}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="stagiaire__action">
+        <div className="stagiaire__min_note">
+          <label htmlFor="note">
+            La plus petite note de bac pout l'option :{" "}
           </label>
-          <div className="form__group__body">
-            <input
-              type="text"
-              placeholder="Enter a value"
-              name="value"
-              onChange={handleChange}
-            />
-          </div>
-        </div>
-        <div className="form__group">
-          <label htmlFor="" className="form__group__title">
-            Style
-          </label>
-          <div className="form__group__body">
-            <select name="style" id="" onChange={handleChange}>
-              {data.style.map((item) => (
-                <option key={item} value={item}>
+          <select name="" id="note" onChange={handleNote}>
+            <option value="">Chose option...</option>
+            {[...new Set(stagiaires.map((item) => item.Option))].map(
+              (item, index) => (
+                <option value={item} key={index}>
                   {item}
                 </option>
-              ))}
-            </select>
-          </div>
+              )
+            )}
+          </select>
+          <span className="min__note">{minNote}</span>
         </div>
-        <div className="form__group">
-          <label htmlFor="" className="form__group__title">
-            Icon
+        <div className="stagiaire__name_upper">
+          <label htmlFor="upper">
+            Transformer les noms des stagiaires en majuscule :
           </label>
-          <div className="form__group__body">
-            <input
-              list="icon"
-              onChange={handleChange}
-              name="icon"
-              autoComplete="off"
-              placeholder="Chose Icon..."
-            />
-            <datalist id="icon">
-              {data.icon.map((item) => (
-                <option key={item} value={item} />
-              ))}
-            </datalist>
-          </div>
+          <input
+            type="button"
+            value="to Upper Case"
+            onClick={() =>
+              setStagiaires(
+                stagiaires.map((stg) => ({
+                  ...stg,
+                  Nom: stg.Nom.toUpperCase(),
+                }))
+              )
+            }
+          />
         </div>
-        <div className="form__group">
-          <div className="form__group__status">
-            <input
-              type="checkbox"
-              name="active"
-              onChange={(e) =>
-                setControl((prev) => ({ ...prev, active: !control.active }))
-              }
-              value="active"
-              id="active"
-            />
-            <label htmlFor="active">Active</label>
-          </div>
-          <div className="form__group__status">
-            <input
-              type="checkbox"
-              name="disabled"
-              value="disabled"
-              id="disabled"
-              onChange={(e) =>
-                setControl((prev) => ({ ...prev, disabled: !control.disabled }))
-              }
-            />
-            <label htmlFor="disabled">Disabled</label>
-          </div>
+        <div className="stagiaire__add_stg">
+          <label htmlFor="add">Ajouter stagiaire</label>
+          <input
+            type="button"
+            value="Add Stg"
+            onClick={() =>
+              setStagiaires([
+                ...stagiaires,
+                { Nom: "Fatima", NoteBac: 14, Option: "ID" },
+              ])
+            }
+          />
         </div>
-        <div className="form__group">
-          <button type="submit" className="action__create btn btn-primary">
-            <i className="bi bi-lightning-charge-fill"></i>
-            Create
-          </button>
-          <input type="number" name="nbr" id="nbr" placeholder="N°" min={0} />
-          <label htmlFor="nbr" className="form__group__title">
-            Button(s)
-          </label>
+        <div className="stagiaire__moyenne">
+          <label htmlFor="moyenne">la moyenne des notes des stagiaires </label>
+          <input
+            type="button"
+            value="Moyenne"
+            onClick={() =>
+              setMoyenne(
+                stagiaires.reduce(
+                  (total, item) =>
+                    item.NoteBac > 12 ? total + item.NoteBac : total,
+                  0
+                ) / stagiaires.length
+              )
+            }
+          />
+          {moyenne.toFixed(2)}
         </div>
-      </form>
-
-      <div className="result__example">
-        <label htmlFor="">Button example : </label>
-        <button
-          className={`${control.style && "btn btn-" + control.style} ${
-            control.active && "active"
-          }`}
-          disabled={control.disabled ? "disabled" : ""}
-        >
-          {control.icon && <i className={`bi bi-${control.icon}`}></i>}{" "}
-          {control.value}
-        </button>
       </div>
-
-      <div className="result__collection">
-        <div className="collection__title">Buttons Colluction : </div>
-        <div className="collection__result">
-          {controls.map((item, index) => (
-            <div key={index}>
-              {[...Array(+item.nbr).keys()].map((btn_index) => (
-                <Button key={+item.index - item.nbr + btn_index} {...item} />
-              ))}
-            </div>
-          ))}
+      <div className="validation__input">
+        <div className="input__fields">
+          <input type="text" onChange={(e) => setTxt(e.target.value)} />
+          <input type="button" value="Valider" onClick={() => setError(txt)} />
         </div>
-        <div className="collection__count">
-          {controls[controls.length - 1]
-            ? `${controls[controls.length - 1].index} elements`
-            : "no element"}
-        </div>
+        {txt.trim().length >= 10 && (
+          <span className="green">Vous avez bien saisi 10 lettres</span>
+        )}
+        {txt.trim().length < 10 && (
+          <span className="red">Saisir au moins 10 lettres</span>
+        )}
+        <p className="error">{error}</p>
       </div>
     </div>
   );
-};
-
-export default App;
+}
